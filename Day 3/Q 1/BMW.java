4c234fe125028eeeafdf025ead7cd8af12e6dfa4
Yukta03 <72689246+Yukta03@users.noreply.github.com>
2021-10-07 22:08:07 +0530

    Add files via upload

---
+++ Day 3/Q 1/BMW.java
@@ -0,0 +1,17 @@
+package com.hsbc.automobile;
+
+public class BMW extends Car{
+
+	@Override
+	public String getMileage() {
+		// TODO Auto-generated method stub
+		return "Alto mileage: 21kmpl";
+	}
+
+	@Override
+	public double getPrice() {
+		// TODO Auto-generated method stub
+		return 6000000.0;
+	}
+
+}
