90ff083e72f077c31b03bcb95c6cf91235b96c91
Yukta03 <72689246+Yukta03@users.noreply.github.com>
2021-10-12 20:06:45 +0530

    Add files via upload

---
+++ Day 5/EmployeeServiceImpl.java
@@ -0,0 +1,51 @@
+ package com.hsbc.service;
+
+import com.hsbc.beans.Employee;
+import com.hsbc.dao.EmployeeDao;
+import com.hsbc.dao.EmployeeDaoArrayImpl;
+import com.hsbc.exceptions.EmployeeNotFoundException;
+
+public class EmployeeServiceImpl implements EmployeeService {
+	private EmployeeDao dao = null;
+	public EmployeeServiceImpl() {
+		// this is wrong approach we need to use factory pattern
+		dao = new EmployeeDaoArrayImpl();
+	}
+
+	@Override
+	public void store(Employee employee) {
+		dao.save(employee);
+	}
+
+
+	@Override
+	public Employee findEmployee(int id) throws EmployeeNotFoundException {
+	Employee emp = dao.fetchEmployeeById(id);
+		if(emp == null) {
+			throw new EmployeeNotFoundException("Sorry employee with an id " +id+" not found");
+		}
+		return emp;
+	}
+
+	@Override
+	public Employee[] findAllEmployees() {
+		return dao.findAllEmployees();
+	}
+
+	@Override
+	public void updateSalary(int id, double salary) throws EmployeeNotFoundException  {
+		Employee employee = findEmployee(id);
+		employee.setSalary(salary);
+		dao.updateEmployee(id,employee);
+	}
+
+	@Override
+	public void updateName(int id, String name) throws EmployeeNotFoundException {
+		Employee employee = findEmployee(id);
+		employee.setName(name);
+		dao.updateEmployee(id,employee);
+			
+	}
+
+	
+}
